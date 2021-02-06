c28a92b43c562467ae1cbbadf1faf722e1100388
geraldoneto771 <geraldopireslacerda771@gmail.com>
2021-02-06 14:11:42 -0300

    Tela de cadastro finalizada faltando validar cao

--- lib/screens/tela_cadastrar_cao.dart
+++ lib/screens/tela_cadastrar_cao.dart
@@ -12,7 +12,11 @@ import 'package:dogfamily/widgets/botao_anunciar_cao.dart';
 class TelaAnunciarCao extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
+      
+       // backgroundColor: Colors.blue.shade200,
+      
       drawer: CustomDrawerWidget(),
       appBar: AppBar(
         title: Text("Cadastro do cão"),
@@ -25,6 +29,7 @@ class TelaAnunciarCao extends StatelessWidget {
       body: MyCustomForm(),
       
     );
+    
   }
 
   
@@ -36,6 +41,11 @@ class MyCustomForm extends StatefulWidget {
     return MyCustomFormState();
   }
 }
+//BACKGROUND
+
+
+
+//FIM BACKGROUND
 
 // Create a corresponding State class.
 // This class holds data related to the form.
@@ -90,7 +100,7 @@ bool _enabled = false;
                     child: TextField(
                       decoration: InputDecoration(
                         border: InputBorder.none,
-                        hintText: "Raça do cão",
+                        hintText: "Raça",
                         hintStyle: TextStyle(color: Colors.grey[400])
                       ),
                     )
@@ -100,11 +110,21 @@ bool _enabled = false;
                     child: TextField(
                       decoration: InputDecoration(
                         border: InputBorder.none,
-                        hintText: "Idade do cão",
+                        hintText: "Idade",
                         hintStyle: TextStyle(color: Colors.grey[400])
                       ),
                     )
                   ),
+                    Container(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Altura",
+                        hintStyle: TextStyle(color: Colors.grey[400])
+                      ),
+                    )
+                    ),
                   Container(
                     padding: EdgeInsets.all(8.0),
                     child: TextField(
@@ -115,6 +135,7 @@ bool _enabled = false;
                       ),
                     )
                   ),
+
                   Container(
                     padding: EdgeInsets.all(8.0),
                     child: TextField(
@@ -124,17 +145,29 @@ bool _enabled = false;
                         hintStyle: TextStyle(color: Colors.grey[400])
                       ),
                     )
-                    ),
-                     Container( child: Column(
-          children: [
-                        Switch(
+                    )
+                     
+                  
+                  
+                  
+
+                ],
+              )   
+          ),
+          // GENERO DO CAO
+          Row (
+            children: [           
+                                 
+Expanded(flex: 2,child: Switch( 
                             value: _enabled,
+                            
                             onChanged: (check) {
                               setState(() {
                                 _enabled = check;
                               });
-                            }),
-                        RaisedButton(
+                            }),),
+  
+ Expanded(flex: 2,child: RaisedButton(
                           disabledColor: Colors.black,
                           disabledTextColor: Colors.white,
                           onPressed: _enabled
@@ -142,18 +175,21 @@ bool _enabled = false;
                               : () {
                                   print("Clicked");
                                 },
+                         child: Center(
                           child: Text("Macho"),
+                         )
                           
-                          
-                        ),
-                        Switch(
+                        ), ),                
+
+ Expanded(flex: 2, child: Switch(
                             value: _enabled,
                             onChanged: (check) {
                               setState(() {
                                 _enabled = check;
                               });
-                            }),
-                        RaisedButton(
+                            }),),
+                        
+     Expanded(flex: 2, child:RaisedButton(
                           disabledColor: Colors.black,
                           disabledTextColor: Colors.white,
                           onPressed: _enabled
@@ -164,20 +200,122 @@ bool _enabled = false;
                           child: Text("Fêmea"),
                           
                           
-                        ),
-                 ],
-        ),),
-                  
-                  
-                  
+                        ),),                            
+                                              
+ ],                                             
+                 
+        ), // FIM DO GENERO DO CAO  
 
-                ],
-              )   
-          ),
+// DEFICIENCIA
+          Row (
+            children: [           
+                                 
+Expanded(flex: 1,child: Switch( 
+                            value: _enabled,
+                            
+                            onChanged: (check) {
+                              setState(() {
+                                _enabled = check;
+                              });
+                            }),),
+  
+ Expanded(flex: 1,child: RaisedButton(
+                          disabledColor: Colors.black,
+                          disabledTextColor: Colors.white,
+                          onPressed: _enabled
+                              ? null
+                              : () {
+                                  print("Clicked");
+                                },
+                         child: Center(
+                          child: Text("Deficiente"),
+                         )
+                          
+                        ), ),                
+
+ Expanded(flex: 1, child: Switch(
+                            value: _enabled,
+                            onChanged: (check) {
+                              setState(() {
+                                _enabled = check;
+                              });
+                            }),),
+                        
+     Expanded(flex: 1, child:RaisedButton(
+                          disabledColor: Colors.black,
+                          disabledTextColor: Colors.white,
+                          onPressed: _enabled
+                              ? null
+                              : () {
+                                  print("Clicked");
+                                },
+                          child: Text("Não"),
+                          
+                          
+                        ),),                            
+                                              
+ ],                                             
+                 
+        ), // FIM DEFICIENCIA
+
+// REMEDIO
+          Row (
+            children: [           
+                                 
+Expanded(flex: 2,child: Switch( 
+                            value: _enabled,
+                            
+                            onChanged: (check) {
+                              setState(() {
+                                _enabled = check;
+                              });
+                            }),),
+  
+ Expanded(flex: 2,child: RaisedButton(
+                          disabledColor: Colors.black,
+                          disabledTextColor: Colors.white,
+                          onPressed: _enabled
+                              ? null
+                              : () {
+                                  print("Clicked");
+                                },
+                         child: Center(
+                          child: Text("Toma remédio"),
+                         )
+                          
+                        ), ),                
+
+ Expanded(flex: 2, child: Switch(
+                            value: _enabled,
+                            onChanged: (check) {
+                              setState(() {
+                                _enabled = check;
+                              });
+                            }),),
+                        
+     Expanded(flex: 2, child:RaisedButton(
+                          disabledColor: Colors.black,
+                          disabledTextColor: Colors.white,
+                          onPressed: _enabled
+                              ? null
+                              : () {
+                                  print("Clicked");
+                                },
+                          child: Text("Não"),
+                          
+                          
+                        ),),                            
+                                              
+ ],                                             
+                 
+        ), // FIM REMEDIO
+
           SizedBox(height: 30,),
           Container(
+            
           height: 50,
-          decoration: BoxDecoration(
+          
+          decoration: BoxDecoration( 
             borderRadius: BorderRadius.circular(10),
             gradient: LinearGradient(
               colors: [
